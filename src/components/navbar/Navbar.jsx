c33a4be790fe7c0469cd62dfcd72c879b3604aab
amh6281 <amh6281@naver.com>
2023-02-16 12:03:18 +0900

    Drop down menu 구현 2022.02.16
    username 클릭 시 setOpen이 true로 변경되면서 menu가 보여진다.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -16,6 +17,12 @@ const Navbar = () => {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "홍길동",
+    isSeller: true,
+  };
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -31,8 +38,29 @@ const Navbar = () => {
           <span>Explore</span>
           <span>Korean</span>
           <span>SIgn In</span>
-          <span>Seller</span>
-          <button>JoIn</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
       {active && (
