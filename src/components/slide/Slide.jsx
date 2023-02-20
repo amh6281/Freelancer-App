74c9f6b44571dcfb698dd1aadcfee9db1161ad28
amh6281 <amh6281@naver.com>
2023-02-20 18:58:15 +0900

    Slide 기능 구현 2022.02.20
    react-carousel 라이브러리를 이용해 dummydata의 슬라이드 기능을 구현한다.

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./Slide.scss";
 import Slider from "infinite-react-carousel";
 
-const Slide = () => {
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
   return (
     <div className="slide">
-      <div className="container"></div>
+      <div className="container">
+        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+          {children}
+        </Slider>
+      </div>
     </div>
   );
 };
