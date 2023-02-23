98caa4b3a399221f5c5f48582b75aa728db2d764
amh6281 <amh6281@naver.com>
2023-02-23 18:58:12 +0900

    Feature component design 2022.02.23

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,7 +2,50 @@ import React from "react";
 import "./Feature.scss";
 
 const Feature = () => {
-  return <div className="feature">Feature</div>;
+  return (
+    <div className="feature">
+      <div className="container">
+        <div className="item">
+          <h1>지금 필요한 프리랜서를 지금 당장 만나보세요!</h1>
+          <div className="title">
+            <img src="./img/check.png" alt="" />
+            최적의 가격 & 최고의 서비스
+          </div>
+          <p>
+            다양한 가격대에서 고품질 서비스를 골라보세요. 프로젝트 기반 가격
+            책정.
+          </p>
+          <div className="title">
+            <img src="./img/check.png" alt="" />
+            최적의 가격 & 최고의 서비스
+          </div>
+          <p>
+            다양한 가격대에서 고품질 서비스를 골라보세요. 프로젝트 기반 가격
+            책정.
+          </p>
+          <div className="title">
+            <img src="./img/check.png" alt="" />
+            최적의 가격 & 최고의 서비스
+          </div>
+          <p>
+            다양한 가격대에서 고품질 서비스를 골라보세요. 프로젝트 기반 가격
+            책정.
+          </p>
+          <div className="title">
+            <img src="./img/check.png" alt="" />
+            최적의 가격 & 최고의 서비스
+          </div>
+          <p>
+            다양한 가격대에서 고품질 서비스를 골라보세요. 프로젝트 기반 가격
+            책정.
+          </p>
+        </div>
+        <div className="item">
+          <video src="./img/video.mp4" controls></video>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
