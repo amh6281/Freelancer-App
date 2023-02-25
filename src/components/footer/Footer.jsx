d2833cab6a73521d7d9761fecb31dbe2fe01d36e
amh6281 <amh6281@naver.com>
2023-02-25 14:20:54 +0900

    Footer component design 2022.02.25

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,95 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Category</h2>
+            <span>Graphic & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Freelancer</span>
+            <span>Buying on Freelancer</span>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Event</span>
+            <span>Blog</span>
+            <span>Influencer</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className="item">
+            <h2>More From Freelancer</h2>
+            <span>Freelancer Business</span>
+            <span>Freelancer Pro</span>
+            <span>Freelancer Logo Maker</span>
+            <span>Freelancer Guides</span>
+            <span>Get Inspired</span>
+            <span>Freelancer Select</span>
+            <span>ClearVoice</span>
+            <span>Freelancer Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>Freelancer</h2>
+            <span>© Freelancer International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="./img/language.png" alt="" />
+              <span>Korean</span>
+            </div>
+            <div className="link">
+              <img src="./img/coin.png" alt="" />
+              <span>KRW</span>
+            </div>
+            <img src="./img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
