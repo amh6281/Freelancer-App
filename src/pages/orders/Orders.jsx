ee7e519256e83df3c7bd0683227f1b9e950b0cb4
amh6281 <amh6281@naver.com>
2023-03-07 19:30:40 +0900

    Orders page design 2022.03.07

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,7 +1,106 @@
 import React from "react";
+import "./Orders.scss";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const currentUser = {
+    id: 1,
+    username: "Anna",
+    isSeller: true,
+  };
+
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>주문</h1>
+        </div>
+        <table>
+          <tr>
+            <th>썸네일</th>
+            <th>제목</th>
+            <th>가격</th>
+            <th>{currentUser?.isSeller ? "구매자" : "판매자"}</th>
+            <th>연락</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>39,000원</td>
+            <td>홍길동</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>39,000원</td>
+            <td>Soojin</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>39,000원</td>
+            <td>철수님</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>39,000원</td>
+            <td>하리보</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>39,000원</td>
+            <td>Minsoo</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
