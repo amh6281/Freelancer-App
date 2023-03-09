12b49204b312c16b6cfab9f45b816ecd781ded87
amh6281 <amh6281@naver.com>
2023-03-09 19:52:32 +0900

    Message page owner 추가 2022.03.09

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -25,15 +25,185 @@ const Message = () => {
               laboriosam nisi animi! Error, tenetur!
             </p>
           </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
         </div>
+        <hr />
         <div className="write">
           <textarea
             name=""
-            placeholder="write a message"
+            placeholder="메시지를 입력하세요."
             id=""
             cols="30"
             rows="10"
-          ></textarea>
+          />
+          <button>전송</button>
         </div>
       </div>
     </div>
