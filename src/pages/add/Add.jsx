966bd4cbf524ef655725e4f9ccb5171b63b3d3b5
amh6281 <amh6281@naver.com>
2023-03-10 19:15:58 +0900

    Add page 구성요소 추가 2022.03.10

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Add = () => {
-  return <div className="add">Add</div>;
+  return (
+    <div className="add">
+      <div className="container">
+        <h1>새 프로젝트 추가</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">제목</label>
+            <input type="text" />
+            <label htmlFor="">카테고리</label>
+            <select name="cats" id="cats">
+              <option value="design">Design</option>
+              <option value="web">Web Development</option>
+              <option value="animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
