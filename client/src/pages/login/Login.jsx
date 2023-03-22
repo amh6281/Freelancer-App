3ac5448bdd1903ec7b3c7c64fc05f26267db8313
amh6281 <amh6281@naver.com>
2023-03-22 12:12:58 +0900

    Login page 로그인 api 연동 2022.03.22
    Login page에서 login api를 연동한다.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/login", {
+        username,
+        password,
+      });
+      console.log(res.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>SIGN IN</h2>
         <label htmlFor="">아이디</label>
-        <input name="username" type="text" />
+        <input
+          name="username"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label htmlFor="">비밀번호</label>
-        <input name="password" type="password" />
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">로그인</button>
       </form>
     </div>
