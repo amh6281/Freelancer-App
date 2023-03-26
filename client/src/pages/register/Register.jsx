327ee3e17a89a210d232ca4f697f0182f986f075
amh6281 <amh6281@naver.com>
2023-03-26 10:15:31 +0900

    Register page input value 값 가져오기 2022.03.26

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.scss";
 
 const Register = () => {
+  const [file, setFile] = useState(null);
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    img: "",
+    country: "",
+    isSeller: false,
+    desc: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSeller = (e) => {
+    setUser((prev) => {
+      return { ...prev, isSeller: e.target.checked };
+    });
+  };
+
+  console.log(user);
   return (
     <div className="register">
       <form>
         <div className="left">
           <h1>회원가입</h1>
           <label htmlFor="">아이디</label>
-          <input name="username" type="text" placeholder="test" />
+          <input
+            name="username"
+            type="text"
+            placeholder="test"
+            onChange={handleChange}
+          />
           <label htmlFor="">이메일</label>
-          <input name="email" type="email" placeholder="email@email.com" />
+          <input
+            name="email"
+            type="email"
+            placeholder="email@email.com"
+            onChange={handleChange}
+          />
           <label htmlFor="">비밀번호</label>
-          <input name="password" type="password" />
+          <input name="password" type="password" onChange={handleChange} />
           <label htmlFor="">프로필 사진</label>
-          <input type="file" />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
           <label htmlFor="">국가</label>
-          <input name="country" type="text" placeholder="한국" />
+          <input
+            name="country"
+            type="text"
+            placeholder="한국"
+            onChange={handleChange}
+          />
           <button type="submit">가입하기</button>
         </div>
         <div className="right">
@@ -24,12 +63,17 @@ const Register = () => {
           <div className="toggle">
             <label htmlFor="">판매자 계정 활성화</label>
             <label className="switch">
-              <input type="checkbox" />
+              <input type="checkbox" onChange={handleSeller} />
               <span className="slider round"></span>
             </label>
           </div>
           <label htmlFor="">휴대폰 번호</label>
-          <input name="phone" type="text" placeholder="010-1234-5678" />
+          <input
+            name="phone"
+            type="text"
+            placeholder="010-1234-5678"
+            onChange={handleChange}
+          />
           <label htmlFor="">설명</label>
           <textarea
             placeholder="짧은 자기소개"
@@ -37,7 +81,8 @@ const Register = () => {
             id=""
             cols="30"
             rows="10"
-          ></textarea>
+            onChange={handleChange}
+          />
         </div>
       </form>
     </div>
