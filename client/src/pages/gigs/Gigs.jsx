e9ad27e64c39218390b4d06fe510fd296dea95c6
amh6281 <amh6281@naver.com>
2023-04-03 20:13:05 +0900

    React query gigs api 호출 2022.04.03
    React query를 사용하여 gigs data fetch

--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 import "./Gigs.scss";
 import GigCard from "../../components/gigCard/GigCard";
 import { gigs } from "../../data";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const Gigs = () => {
   const [sort, setSort] = useState("sales");
   const [open, setOpen] = useState(false);
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () => newRequest("/gigs"),
+  });
+
+  console.log(data);
 
   const reSort = (type) => {
     setSort(type);
