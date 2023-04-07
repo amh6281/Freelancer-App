c37774dd3452f2eba968962ab8ae9dd1fc489451
amh6281 <amh6281@naver.com>
2023-04-07 19:57:56 +0900

    Gig page userInfo fetch 2022.04.07
    useQuery를 통해 gig의 userId를 가져오고, enabled 속성을 사용하여 userId가 있을 경우에만 쿼리를 요청한다.

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -8,14 +8,29 @@ import newRequest from "../../utils/newRequest";
 function Gig() {
   const { id } = useParams();
 
-  const { isLoading, error, data, refetch } = useQuery({
+  const { isLoading, error, data } = useQuery({
     queryKey: ["gig"],
     queryFn: () =>
       newRequest.get(`/gigs/single/${id}`).then((res) => {
         return res.data;
       }),
   });
-  console.log(data);
+
+  const userId = data?.userId;
+
+  const {
+    isLoading: isLoadingUser,
+    error: errorUser,
+    data: dataUser,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: () =>
+      newRequest.get(`/users/${userId}`).then((res) => {
+        return res.data;
+      }),
+    enabled: !!userId, //userId가 존재할 경우에만 쿼리 요청
+  });
+
   return (
     <div className="gig">
       {isLoading ? (
@@ -27,24 +42,30 @@ function Gig() {
           <div className="left">
             <span className="breadCrumbs">Freelancer &gt; {data.cat} &gt;</span>
             <h1>{data.title}</h1>
-            <div className="user">
-              <img
-                className="pp"
-                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <span>Kim soojin</span>
-              {!isNaN(data.totalStars / data.starNumber) && (
-                <div className="stars">
-                  {Array(Math.round(data.totalStars / data.starNumber))
-                    .fill()
-                    .map((item, i) => (
-                      <img src="/img/star.png" alt="" key={i} />
-                    ))}
-                  <span>{Math.round(data.totalStars / data.starNumber)}</span>
-                </div>
-              )}
-            </div>
+            {isLoadingUser ? (
+              "loading..."
+            ) : errorUser ? (
+              "잘못된 접근입니다."
+            ) : (
+              <div className="user">
+                <img
+                  className="pp"
+                  src={dataUser.img || "/img/noavatar.jpg"}
+                  alt=""
+                />
+                <span>{dataUser.username}</span>
+                {!isNaN(data.totalStars / data.starNumber) && (
+                  <div className="stars">
+                    {Array(Math.round(data.totalStars / data.starNumber))
+                      .fill()
+                      .map((item, i) => (
+                        <img src="/img/star.png" alt="" key={i} />
+                      ))}
+                    <span>{Math.round(data.totalStars / data.starNumber)}</span>
+                  </div>
+                )}
+              </div>
+            )}
             <Slider slidesToShow={1} arrowsScroll={1} className="slider">
               {data.images.map((img) => (
                 <img key={img} src={img} alt="" />
@@ -52,61 +73,60 @@ function Gig() {
             </Slider>
             <h2>작품 소개</h2>
             <p>{data.desc}</p>
-            <div className="seller">
-              <h2>프리랜서 소개</h2>
-              <div className="user">
-                <img
-                  src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Kim soojin</span>
-                  {!isNaN(data.totalStars / data.starNumber) && (
-                    <div className="stars">
-                      {Array(Math.round(data.totalStars / data.starNumber))
-                        .fill()
-                        .map((item, i) => (
-                          <img src="/img/star.png" alt="" key={i} />
-                        ))}
-                      <span>
-                        {Math.round(data.totalStars / data.starNumber)}
-                      </span>
+            {isLoadingUser ? (
+              "loading..."
+            ) : errorUser ? (
+              "잘못된 접근입니다."
+            ) : (
+              <div className="seller">
+                <h2>프리랜서 소개</h2>
+                <div className="user">
+                  <img src={dataUser.img || "/img/noavatar.jpg"} alt="" />
+                  <div className="info">
+                    <span>{dataUser.username}</span>
+                    {!isNaN(data.totalStars / data.starNumber) && (
+                      <div className="stars">
+                        {Array(Math.round(data.totalStars / data.starNumber))
+                          .fill()
+                          .map((item, i) => (
+                            <img src="/img/star.png" alt="" key={i} />
+                          ))}
+                        <span>
+                          {Math.round(data.totalStars / data.starNumber)}
+                        </span>
+                      </div>
+                    )}
+                    <button>문의하기</button>
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="items">
+                    <div className="item">
+                      <span className="title">국적</span>
+                      <span className="desc">{dataUser.country}</span>
                     </div>
-                  )}
-                  <button>문의하기</button>
+                    <div className="item">
+                      <span className="title">활동 기간</span>
+                      <span className="desc">2022.08</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">평균 제작 시간</span>
+                      <span className="desc">4시간</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">최근 의뢰</span>
+                      <span className="desc">하루 전</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">언어</span>
+                      <span className="desc">한국어</span>
+                    </div>
+                  </div>
+                  <hr />
+                  <p>{dataUser.desc}</p>
                 </div>
               </div>
-              <div className="box">
-                <div className="items">
-                  <div className="item">
-                    <span className="title">국적</span>
-                    <span className="desc">대한민국</span>
-                  </div>
-                  <div className="item">
-                    <span className="title">활동 기간</span>
-                    <span className="desc">2022.08</span>
-                  </div>
-                  <div className="item">
-                    <span className="title">평균 제작 시간</span>
-                    <span className="desc">4시간</span>
-                  </div>
-                  <div className="item">
-                    <span className="title">최근 의뢰</span>
-                    <span className="desc">하루 전</span>
-                  </div>
-                  <div className="item">
-                    <span className="title">언어</span>
-                    <span className="desc">한국어</span>
-                  </div>
-                </div>
-                <hr />
-                <p>
-                  안녕하세요! AI 아티스트 김수진입니다! 저는 여가시간에 AI
-                  아트를 만드는 것을 좋아합니다! AI 프로그램 경험이 풍부하여
-                  결과물이 마음에 들 것이라 확신할 수 있습니다.
-                </p>
-              </div>
-            </div>
+            )}
             <div className="reviews">
               <h2>리뷰</h2>
               <div className="item">
