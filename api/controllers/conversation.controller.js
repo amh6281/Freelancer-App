272afe144d90f23c91231f15b849969a962e19d7
amh6281 <amh6281@naver.com>
2023-04-15 13:17:11 +0900

    Get single conversation api 구현 2022.04.15

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -37,3 +37,12 @@ export const updateConversation = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getSingleConversation = async (req, res, next) => {
+  try {
+    const conversation = await Conversation.findOne({ id: req.params.id });
+    res.status(200).send(conversation);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
 import {
   createConversation,
+  getSingleConversation,
   updateConversation,
 } from "../controllers/conversation.controller.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/", verifyToken, createConversation);
 router.put("/:id", verifyToken, updateConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
 
 export default router;
