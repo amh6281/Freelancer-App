1f03436edb7402aa96c042c4b47335ab208e6079
amh6281 <amh6281@naver.com>
2023-04-21 16:06:02 +0900

    결제 api 구현 2022.04.21
    Stripe api를 활용한 결제 api를 생성한다.

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,26 +1,35 @@
 import { createError } from "../utils/createError.js";
 import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
+import Stripe from "stripe";
 
-export const createOrder = async (req, res, next) => {
-  try {
-    const gig = await Gig.findById(req.params.gigId);
+export const paymentIntent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
 
-    const newOrder = new Order({
-      gigId: gig._id,
-      img: gig.cover,
-      title: gig.title,
-      buyerId: req.userId,
-      sellerId: gig.userId,
-      price: gig.price,
-      payment_intent: "temporary",
-    });
+  const gig = await Gig.findById(req.params.id);
 
-    await newOrder.save();
-    res.status(200).send("성공");
-  } catch (err) {
-    next(err);
-  }
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "krw",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 };
 
 export const getOrders = async (req, res, next) => {
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
-import { createOrder, getOrders } from "../controllers/order.controller.js";
+import { getOrders, paymentIntent } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
-router.post("/:gigId", verifyToken, createOrder);
 router.get("/", verifyToken, getOrders);
+router.post("/create-payment-intent/:id", verifyToken, paymentIntent);
 
 export default router;
