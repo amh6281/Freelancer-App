2256483bac49bc0d91fff85e0a8261b81f4bfec9
amh6281 <amh6281@naver.com>
2023-04-22 11:46:33 +0900

    Order confirm api 호출 2022.04.22
    주문 완료시 payment_intent를 통해 isCompleted true로 업데이트 후 4초뒤 주문페이지로 이동한다.

--- client/src/pages/success/Success.jsx
+++ client/src/pages/success/Success.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
 
 const Success = () => {
-  return <div>Success</div>;
+  const navigate = useNavigate();
+
+  //payment_intent 가져오기
+  const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  const payment_intent = params.get("payment_intent");
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        await newRequest.put("/orders", { payment_intent });
+        setTimeout(() => {
+          navigate("/orders");
+        }, 4000);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
+
+  return (
+    <div>4초 뒤, 주문 확인 페이지로 이동합니다. 페이지를 닫지 마세요.</div>
+  );
 };
 
 export default Success;
