fdd9e15b1c5b45ad161e8d68188a3b7664a2e5a5
amh6281 <amh6281@naver.com>
2023-04-24 11:46:13 +0900

    MyGigs page data fetch 및 delete 기능 적용 2022.04.24
    currentUser._id를 통해 gigs를 가져오고, delete 기능을 적용한다.

--- client/src/pages/myGigs/MyGigs.jsx
+++ client/src/pages/myGigs/MyGigs.jsx
@@ -1,102 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./MyGigs.scss";
+import getCurrentUser from "../../utils/getCurrentUser";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const MyGigs = () => {
+  const currentUser = getCurrentUser();
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["myGigs"],
+    queryFn: () =>
+      newRequest.get(`/gigs?userId=${currentUser._id}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/gigs/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className="myGigs">
-      <div className="container">
-        <div className="title">
-          <h1>프로젝트</h1>
-          <Link to="/add">
-            <button>새 프로젝트 추가</button>
-          </Link>
+      {isLoading ? (
+        "loading..."
+      ) : error ? (
+        "잘못된 접근입니다."
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>프로젝트</h1>
+            <Link to="/add">
+              <button>새 프로젝트 추가</button>
+            </Link>
+          </div>
+          <table>
+            <tr>
+              <th>썸네일</th>
+              <th>제목</th>
+              <th>가격</th>
+              <th>판매량</th>
+              <th>상태</th>
+            </tr>
+            {data.map((gig) => (
+              <tr key={gig._id}>
+                <td>
+                  <img className="image" src={gig.cover} alt="" />
+                </td>
+                <td>{gig.title}</td>
+                <td>{gig.price}원</td>
+                <td>{gig.sales}개</td>
+                <td>
+                  <img
+                    className="delete"
+                    src="/img/delete.png"
+                    alt=""
+                    onClick={() => handleDelete(gig._id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>썸네일</th>
-            <th>제목</th>
-            <th>가격</th>
-            <th>판매량</th>
-            <th>상태</th>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>39,000원</td>
-            <td>3개</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>39,000원</td>
-            <td>3개</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>39,000원</td>
-            <td>3개</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>39,000원</td>
-            <td>3개</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>39,000원</td>
-            <td>3개</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
