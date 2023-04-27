65989aead079e7ef71ff62a708d0ad01735577e0
amh6281 <amh6281@naver.com>
2023-04-27 19:46:56 +0900

    File upload & features add 기능 구현 2022.04.27

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -1,6 +1,7 @@
 import React, { useReducer, useState } from "react";
 import "./Add.scss";
 import { gigReducer, INITIAL_STATE } from "../../reducers/gigReducer";
+import upload from "../../utils/upload";
 
 const Add = () => {
   const [singleFile, setSingleFile] = useState(undefined);
@@ -16,6 +17,33 @@ const Add = () => {
     });
   };
 
+  const handleFeature = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+
+  const handleUpload = async () => {
+    setUploading(true);
+    try {
+      const cover = await upload(singleFile);
+
+      const images = await Promise.all(
+        [...files].map(async (file) => {
+          const url = await upload(file);
+          return url;
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(files);
   return (
     <div className="add">
       <div className="container">
@@ -31,10 +59,22 @@ const Add = () => {
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
-            <label htmlFor="">커버 이미지</label>
-            <input type="file" />
-            <label htmlFor="">업로드 이미지</label>
-            <input type="file" multiple />
+            <div className="images">
+              <div className="imagesInputs">
+                <label htmlFor="">커버 이미지</label>
+                <input
+                  type="file"
+                  onChange={(e) => setSingleFile(e.target.files[0])}
+                />
+                <label htmlFor="">업로드 이미지</label>
+                <input
+                  type="file"
+                  multiple
+                  onChange={(e) => setFiles(e.target.files)}
+                />
+              </div>
+              <button>{uploading ? "uploading..." : "Upload"}</button>
+            </div>
             <label htmlFor="">설명</label>
             <textarea
               name="desc"
@@ -72,7 +112,10 @@ const Add = () => {
               onChange={handleChange}
             />
             <label htmlFor="">기능 추가</label>
-            <input type="text" placeholder="ex) 페이지 디자인" />
+            <form action="" onSubmit={handleFeature}>
+              <input type="text" placeholder="ex) 페이지 디자인" />
+              <button type="submit">추가</button>
+            </form>
             <label htmlFor="">가격</label>
             <input type="number" name="price" onChange={handleChange} />
           </div>
