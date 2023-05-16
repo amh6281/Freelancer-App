82c258c7d9462fbdbda03606aad4ab223db90468
amh6281 <amh6281@naver.com>
2023-05-16 15:38:21 +0900

    MyGig page 기존 gig data 렌더링 2022.05.16
    수정하기위한 기존의 gig data를 렌더링한다.

--- client/src/pages/myGig/MyGig.jsx
+++ client/src/pages/myGig/MyGig.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./MyGig.scss";
 import { useLocation } from "react-router-dom";
+import { INITIAL_STATE, gigReducer } from "../../reducers/gigReducer";
 
 const MyGig = () => {
   const location = useLocation();
   const gig = location.state;
-  console.log(gig);
+
+  const [state, dispatch] = useReducer(gigReducer, gig);
+
+  const handleFeature = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+
   return (
     <div className="myGig">
       <div className="container">
-        <h1>새 프로젝트 추가</h1>
+        <h1>프로젝트 업데이트</h1>
         <div className="sections">
           <div className="left">
             <label htmlFor="">제목</label>
-            <input type="text" name="title" />
+            <input type="text" name="title" placeholder={gig.title} />
             <label htmlFor="">카테고리</label>
             <select name="cat" id="cat">
+              <option selected>-- {gig.cat} --</option>
               <option value="design">Design</option>
               <option value="web">Web Development</option>
               <option value="animation">Animation</option>
@@ -28,7 +41,7 @@ const MyGig = () => {
                 <label htmlFor="">업로드 이미지</label>
                 <input type="file" multiple />
               </div>
-              <button></button>
+              <button>생성</button>
             </div>
             <label htmlFor="">설명</label>
             <textarea
@@ -36,39 +49,54 @@ const MyGig = () => {
               id=""
               cols="30"
               rows="16"
-              placeholder="고객에게 서비스를 소개하기 위한 설명"
+              placeholder={gig.desc}
             />
             <button>생성</button>
           </div>
           <div className="right">
             <label htmlFor="">서비스 제목</label>
-            <input
-              type="text"
-              name="shortTitle"
-              placeholder="ex) 웹 디자인 1페이지"
-            />
+            <input type="text" name="shortTitle" placeholder={gig.shortTitle} />
             <label htmlFor="">짧은 설명</label>
             <textarea
               name="shortDesc"
               id=""
-              placeholder="고객을 위한 짧은 서비스 설명"
+              placeholder={gig.shortDesc}
               cols="30"
               rows="10"
             />
-            <label htmlFor="">소요 시간 ex) 3일 </label>
-            <input type="number" name="deliveryTime" />
+            <label htmlFor="">소요 시간 (일)</label>
+            <input
+              type="number"
+              name="deliveryTime"
+              placeholder={gig.deliveryTime}
+            />
             <label htmlFor="">수정 가능 횟수</label>
-            <input type="number" name="revisionNumber" />
+            <input
+              type="number"
+              name="revisionNumber"
+              placeholder={gig.revisionNumber}
+            />
             <label htmlFor="">기능 추가</label>
-            <form className="add" action="">
+            <form className="add" action="" onSubmit={handleFeature}>
               <input type="text" placeholder="ex) 페이지 디자인" />
               <button type="submit">추가</button>
             </form>
             <div className="addedFeatures">
-              <div className="item"></div>
+              {state?.features?.map((f) => (
+                <div className="item" key={f}>
+                  <button
+                    onClick={() =>
+                      dispatch({ type: "REMOVE_FEATURE", payload: f })
+                    }
+                  >
+                    {f}
+                    <span>X</span>
+                  </button>
+                </div>
+              ))}
             </div>
             <label htmlFor="">가격</label>
-            <input type="number" name="price" />
+            <input type="number" name="price" placeholder={gig.price} />
           </div>
         </div>
       </div>
